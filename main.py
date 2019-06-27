0ac217001889c30a3591149c99f5ed0ab6365d51
Serhii Charykov <serhii.charykov@gmail.com>
2019-06-27 18:48:36 +0300

    Add static files. For now serve from flask

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -19,4 +19,9 @@ with open('world_cities.csv', newline='') as csvfile:
 def hello():
     city_row = random.choice(cities)
     return render_template('index.html', city=city_row[0], province=city_row[2], country=city_row[1])
+
+
+@app.route('/favicon.svg')
+def root():
+    return app.send_static_file('favicon.svg')
 
